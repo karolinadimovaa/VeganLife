7980cf2d7a87c37db8736c7aed4f16a32845b0bd
Karolina Dimova <70309331+karolinadimovaa@users.noreply.github.com>
2022-04-24 22:23:23 +0200

    Final commit

--- Web/VeganLife.Web/Controllers/AboutUsController.cs
+++ Web/VeganLife.Web/Controllers/AboutUsController.cs
@@ -11,22 +11,13 @@ namespace VeganLife.Web.Controllers
 {
     public class AboutUsController : Controller
     {
-        private readonly ICategoriesService categoriesService;
-        private readonly IRecipesService recipesService;
-        private readonly UserManager<ApplicationUser> userManager;
-
-        public AboutUsController(ICategoriesService categoriesService, IRecipesService recipesService, UserManager<ApplicationUser> userManager)
+        public AboutUsController()
         {
-            this.categoriesService = categoriesService;
-            this.recipesService = recipesService;
-            this.userManager = userManager;
         }
 
         public IActionResult AboutUs()
         {
-            var viewModel = new CreateRecipeInputModel();
-            viewModel.CategoriesItems = this.categoriesService.GetAllAsKeyValuePairs();
-            return this.View(viewModel);
+            return this.View();
         }
     }
 }
